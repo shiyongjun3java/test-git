d8fe7c0e586de5cd8db4ca717067a2a212c981aa
shiyongjun <shiyongjun3java@163.com>
2019-04-20 14:54:50 +0800

    first commit

---
+++ src/test/Hello.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author sunny
+ * @title: Hello
+ * @date 2019-04-2014:36
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(111111);
+    }
+}
